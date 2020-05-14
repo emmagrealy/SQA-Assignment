a9cb46bfb83586183634a22458aff3a86efddcf8
emmagrealy <44479033+emmagrealy@users.noreply.github.com>
2020-05-14 12:38:36 +0100

    Modified : Questions class takes in SurveyResponse Obj

--- SQA/src/Questions.java
+++ SQA/src/Questions.java
@@ -2,10 +2,12 @@ public class Questions {
 
 	//Question
 	private String question;
+	private SurveyResponse response;
 
 	//Constructor
 	public Questions(String questions) {
 		this.question = question;
+		this.response = new SurveyResponse();
 	}
 
 	//Getters and setters
@@ -15,6 +17,14 @@ public class Questions {
 
 	public void setQuestions(String question) {
 		this.question = question;
+	}
+	
+	public SurveyResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(SurveyResponse response) {
+		this.response = response;
 	}
 
 
