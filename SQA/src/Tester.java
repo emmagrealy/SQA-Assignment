385a290d5db725d957cdf362e9ddcff93c9e3d92
emmagrealy <44479033+emmagrealy@users.noreply.github.com>
2020-05-14 12:19:37 +0100

    TEST : Are questions being added to survey? Not passing yet

--- SQA/src/Tester.java
+++ SQA/src/Tester.java
@@ -37,7 +37,7 @@ public class Tester {
 	public void surveyQuestions()
 	{		
 		//Question object
-		Questions one = new Questions("Customer Service");
+		Questions one = new Questions("Our Customer Service");
 		//Collection 
 		ArrayList<Questions> questions = new ArrayList<Questions>();
 		//Add question to list
@@ -45,5 +45,21 @@ public class Tester {
 
 		Survey survey = new Survey("My Questions" ,questions);
 		assertTrue("Should be an object of type 'ArrayList'",survey.getQuestions() instanceof ArrayList);
+	}
+	
+	
+	//Test - check questions are being added to survey
+	@Test //Test Attribute
+	public void addQuestion()
+	{
+		//Question object
+		Questions one = new Questions("Our Customer Service");
+		Questions two = new Questions("Our Product Quality");
+
+		Survey survey = new Survey("My Questions");
+		survey.add(one);
+		survey.add(two);
+
+		assertEquals("Length of questions array should be 2",2 ,survey.getQuestions().size());
 	}
 }
