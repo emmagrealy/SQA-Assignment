6856a73dbb9b7d6238ed58b05ec9c137a461161e
emmagrealy <44479033+emmagrealy@users.noreply.github.com>
2020-05-17 13:40:28 +0100

    Created : SurveyResponse Class
     SurveyResponses-DEV

---
+++ SQA/src/SurveyResponse.java
@@ -0,0 +1,24 @@
+public class SurveyResponse {
+
+	//int value for survey response answer
+	private int answer =0;
+
+	//Empty constructor
+	public SurveyResponse() 
+	{
+
+
+	}
+
+
+	//Getters and setters 
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+
+
+} 
