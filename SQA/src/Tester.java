ec91465b8c22271322d62a367c34ffb095142f09
emmagrealy <44479033+emmagrealy@users.noreply.github.com>
2020-05-18 11:43:18 +0100

    TEST : Can we get a survey answer from the survey? - NOT passing

--- SQA/src/Tester.java
+++ SQA/src/Tester.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -99,5 +100,33 @@ public class Tester {
 
 		//Test - get first questions response answer
 		assertEquals("Value should be '2'",2,one.getResponse().getAnswer());
+	}
+	
+	
+	//Test - is a survey responses obtained from survey 
+	public void surveyResponse()
+	{
+		//Survey
+		Survey survey = new Survey();
+
+		//Question objectS
+		Questions one = new Questions("Our Customer Service");
+		Questions two = new Questions("Our Product Quality");
+
+		//Add answer to question response
+		one.getResponse().setAnswer(2);
+		two.getResponse().setAnswer(3);
+
+
+		//Add questions to survey
+		survey.add(one);
+		survey.add(two);
+
+		//ArrayList being returned
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(2,3));
+
+		//Test - do the response lists match
+		assertArrayEquals("ArrayList should equal [2,3]", expected,survey.getResponses());
+
 	}
 } 
